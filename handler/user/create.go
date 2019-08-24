224a68f892920e58ca9f8ce62da7ec1da6838e62
liyazhong <lyz12918@163.com>
2019-08-24 15:00:46 +0800

    http request and response

--- handler/user/create.go
+++ handler/user/create.go
@@ -2,39 +2,43 @@ package user
 
 import (
 	"fmt"
+	"fruit_shop/handler"
 	"fruit_shop/pkg/errno"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
 )
 
 func Create(c *gin.Context) {
-	var u struct {
-		Username string `json:"username"`
-		Password string `json: "password"`
-	}
+	var u CreateRequest
 
-	var err error
 	if err := c.Bind(&u); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		handler.SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
+	name := c.Param("username")
+	log.Infof("URL username: %s", name)
+
+	desc := c.Query("desc")
+	log.Infof("URL key param desc: %s", desc)
+
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content-Type: %s", contentType)
+
 	log.Debugf("username is: [%s], password is: [%s]", u.Username, u.Password)
 	if u.Username == "" {
-		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not be found in the db,")).Add("This is the additional message")
-		log.Errorf(err, "Get an error")
-	}
-
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
+		handler.SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not be found in the db,")), nil)
+		return
 	}
 
 	if u.Password == "" {
-		err = fmt.Errorf("password is empty")
+		handler.SendResponse(c, fmt.Errorf("password is empty"), nil)
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	resp := CreateResponse{
+		Username: u.Username,
+	}
+
+	handler.SendResponse(c, nil, resp)
 }
